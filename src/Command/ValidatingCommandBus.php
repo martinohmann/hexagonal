a49021d457735033e49d865eb4dd81012331853d
Martin Ohmann <martin.ohmann@lesara.de>
2018-10-21 10:39:12 +0200

    [master] update license header

--- src/Command/ValidatingCommandBus.php
+++ src/Command/ValidatingCommandBus.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the hexagonal package.
  *
- * (c) Martin Ohmann <martin@mohmann.de>
+ * (c) 2018 Martin Ohmann <martin@mohmann.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
